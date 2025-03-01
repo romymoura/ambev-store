d200dea53ea1aabb1d4301edf081518dca137c4e
romy.moura <romy.moura23@gmail.com>
2025-02-28 23:33:57 -0300

    hotfix (discount): Add roles discaount

--- virtual-store/src/app/core/services/auth.service.ts
+++ virtual-store/src/app/core/services/auth.service.ts
@@ -61,6 +61,7 @@ export class AuthService {
   }
 
   logout(): void {
+    localStorage.removeItem('cart');
     localStorage.removeItem('token');
     localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
